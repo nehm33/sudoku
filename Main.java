391f9d9cb972909bc79150a438589d512244758e
yaoe <emmanuel.yao@grenoble-inp.org>
2020-02-04 21:55:49 +0100

    Ajout de fichiers texte de sudoku

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		InputOutputSudoku ios = new InputOutputSudoku("/home/yao/Documents/projets/sudoku/sudoku.txt");
+		InputOutputSudoku ios = new InputOutputSudoku("/home/yao/Documents/projets/sudoku/sudoku2.txt");
 		//InputOutputSudoku ios = new InputOutputSudoku("/user/7/yaoe/Documents/sudoku.txt");
 		System.out.println(ios);
 		ios.resoudreGrille();
